921b32d4180db464c7fa137fbae7a475b6329ae1
Jack <1783461699@qq.com>
2019-08-04 22:08:21 +0800

    config无需重启,调用接口http://localhost:8081/actuator/refresh自动刷新

--- src/main/java/com/cs/configclient/controller/TestController.java
+++ src/main/java/com/cs/configclient/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.cs.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/test", produces = "application/json;charset=UTF-8")
 public class TestController {
@@ -12,8 +14,16 @@ public class TestController {
     @Value("${name}")
     private String name;
 
+    @Value("${refresh}")
+    private String refresh;
+
     @GetMapping("/getName")
     public String testLogin() {
         return name;
+    }
+
+    @GetMapping("/refresh")
+    public String getPort() {
+        return refresh;
     }
 }
